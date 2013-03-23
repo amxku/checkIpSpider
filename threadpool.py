ece4caf4b032880cd5aa37692a188cba5c89bce7
amxku <amxku@sebug.net>
2013-03-23 15:51:19 +0800

    checkIpSpider v0.2

--- threadpool.py
+++ threadpool.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
 """
 Generic thread pool class. Modeled after Java's ThreadPoolExecutor.
 Please note that this ThreadPool does *not* fully implement the PEP 3148
 ThreadPool!
 """
-
 from threading import Thread, Lock, currentThread, Event, Semaphore
 from weakref import ref
 import atexit

a49afded3f7d15b302e64fde2e2c80eb31c8325b
amxku <amxku@sebug.net>
2013-03-23 11:54:41 +0800

    checkIpSpider v0.2

--- checkIpSpider.py
+++ checkIpSpider.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# check ip is spider / amxku@sebug.net  / 2012-03-22 / v0.1
+# check ip is spider / amxku@sebug.net  / 2012-03-22 / v0.2
 #
-import time,re
-from subprocess import Popen,PIPE
+import time,re,socket
 from threadpool import ThreadPool
 
-def checkIpNslookup(arg):
-    cmd = 'nslookup %s' % arg.strip()
-    handle = Popen(cmd, stdout = PIPE, stderr = PIPE, shell = True).stdout
-    re_handle = handle.read()
-    handle.close()
-
-    # name 关键词
-    botkey = ['baidu','google','yahoo','msn']
-
-    if re.search('name',re_handle.lower()):
+def getIpHostName(arg):
+    try:
+        handle = socket.gethostbyaddr(arg.strip())[0]
+        #return any(x in socket.gethostbyaddr(arg.strip())[0] for x in ['baidu','google','yahoo','msn'])
+        # name 关键词
+        botkey = ['baidu','google','yahoo','msn']
         for bk in botkey:
-            checkkey = re.search(bk,re_handle)
+            checkkey = re.search(bk,handle)
             if checkkey:
                 print "%s-%s\n" % (arg.strip(),bk)
                 break
+    except socket.herror:
+        pass
 
 if __name__ == "__main__":
     starttime = time.time()
@@ -28,8 +25,7 @@ if __name__ == "__main__":
     # 开始线程操作
     tp = ThreadPool(10)
     for cip in ipfiles.xreadlines():
-        tp.push(checkIpNslookup, cip)
+        tp.push(getIpHostName, cip)
     tp.wait()
     print tp.busy()
     print 'done ,used :%s' % str(time.time()  - starttime)
-

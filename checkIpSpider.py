756ce014ae79b14bed0fed169b449d7b07c0f90b
amxku <amxku@sebug.net>
2013-03-23 12:57:45 +0800

    checkIpSpider v0.2

--- checkIpSpider.py
+++ checkIpSpider.py
@@ -2,30 +2,26 @@
 # -*- coding:utf-8 -*-
 # check ip is spider / amxku@sebug.net  / 2012-03-22 / v0.2
 #
-import time,re,socket
+import time,socket
 from threadpool import ThreadPool
 
 def getIpHostName(arg):
     try:
-        handle = socket.gethostbyaddr(arg.strip())[0]
-        #return any(x in socket.gethostbyaddr(arg.strip())[0] for x in ['baidu','google','yahoo','msn'])
-        # name 关键词
         botkey = ['baidu','google','yahoo','msn']
-        for bk in botkey:
-            checkkey = re.search(bk,handle)
-            if checkkey:
-                print "%s-%s\n" % (arg.strip(),bk)
-                break
+        handle = socket.gethostbyaddr(arg.strip())[0]
+        if any(x in handle for x in botkey):
+            print "%s-%s\n" % (arg.strip(),handle.split('.')[-2])
     except socket.herror:
         pass
 
 if __name__ == "__main__":
     starttime = time.time()
-    ipfiles = open('ip_list.txt','r')
+    ipfiles = open('ip_list_.txt','r')
     # 开始线程操作
     tp = ThreadPool(10)
     for cip in ipfiles.xreadlines():
         tp.push(getIpHostName, cip)
     tp.wait()
+    ipfiles.close()
     print tp.busy()
     print 'done ,used :%s' % str(time.time()  - starttime)

3fca30a64ac7f305b8b44166d5a8d57e62b6793e
amxku <amxku@sebug.net>
2013-04-13 13:59:16 +0800

    checkIpSpider v0.2

--- checkIpSpider.py
+++ checkIpSpider.py
@@ -2,21 +2,23 @@
 # -*- coding:utf-8 -*-
 # check ip is spider  /  amxku@sebug.net  /  2012-03-22 / v0.2
 #
-import time,socket
+import time
+import socket
 from threadpool import ThreadPool
+
 
 def getIpHostName(arg):
     try:
-        botkey = ['baidu','google','yahoo','msn']
+        botkey = ['baidu', 'google', 'yahoo', 'msn']
         handle = socket.gethostbyaddr(arg)[0]
         if any(x in handle for x in botkey):
-            print "%s-%s\n" % (arg,handle.split('.')[-2])
+            print "%s-%s\n" % (arg, handle.split('.')[-2])
     except socket.herror:
         pass
 
 if __name__ == "__main__":
     starttime = time.time()
-    ipfiles = open('ip_list.txt','r')
+    ipfiles = open('ip_list.txt', 'r')
     # 开始线程操作
     tp = ThreadPool(10)
     for cip in ipfiles.xreadlines():
@@ -24,4 +26,4 @@ if __name__ == "__main__":
     tp.wait()
     ipfiles.close()
     print tp.busy()
-    print 'done ,used :%s' % str(time.time()  - starttime)
+    print 'done ,used :%s' % str(time.time() - starttime)
